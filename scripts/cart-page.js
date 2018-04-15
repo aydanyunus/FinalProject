729dc4911b73384271f1d1b83e26437e66fbb880
aydanyunus <aydanshy@code.edu.az>
2018-04-15 16:42:10 +0400

    Initial commit

---
+++ scripts/cart-page.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".counter span.up").click(function () {
+        var inputvalue = $(".form-input").val();
+        console.log(inputvalue);
+    })
+
+
+})
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+})

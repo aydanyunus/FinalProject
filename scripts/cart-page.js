746b424e682924a8cda92fbf9f9cb6cfbb2e3e49
aydanyunus <aydanshy@code.edu.az>
2018-05-11 17:17:43 +0400

    cart-page and single product js fixed

--- scripts/cart-page.js
+++ scripts/cart-page.js
@@ -2,9 +2,13 @@ $(document).ready(function () {
     var x = 1;
     $(".counter .form-input").attr('value', x);
     $(".up").click(function () {
-        $(".counter .form-input").attr('value', x++);
+        var value = parseInt($(this).parent().children(".form-input").val());
+        value++;
+        $(this).parent().children(".form-input").val(value);
     });
     $(".down").click(function () {
-        $(".counter .form-input").attr('value', x--);
+        var value = parseInt($(this).parent().children(".form-input").val());
+        value--;
+        $(this).parent().children(".form-input").val(value);
     });
 })
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -2,13 +2,34 @@ $(document).ready(function(){
     $('#myTabs a').click(function (e) {
         e.preventDefault()
         $(this).tab('show')
-      });
-      var x = 1;
-      $(".form-input input").attr('value', x);
-      $(".up").click(function () {
-          $(".form-input input").attr('value', x++);
-      });
-      $(".down").click(function () {
-          $(".form-input input").attr('value', x--);
-      });
+    });
+    var x = 1;
+    $(".form-input input").attr('value', x);
+    $(".up").click(function () {
+        $(".form-input input").attr('value', x++);
+    });
+    $(".down").click(function () {
+        $(".form-input input").attr('value', x--);
+    });
+    $(".first").click(function(){
+        $(".four").removeClass("active");
+        $(this).addClass("active");
+        $(".main-img img").attr("src", "images/product-10-421x388.jpg");
+    })
+    $(".second").click(function(){
+        $(".first").removeClass("active");
+        $(this).addClass("active");
+        $(".main-img img").attr("src", "images/product-11-421x388.jpg");
+    })
+    $(".third").click(function(){
+        $(".second").removeClass("active");
+        $(this).addClass("active");
+        $(".main-img img").attr("src", "images/product-12-421x388.jpg");
+    })
+    $(".four").click(function(){
+        $(".third").removeClass("active");
+        $(this).addClass("active");
+        $(".main-img img").attr("src", "images/product-13-421x388.jpg");
+    })
+    
 })
